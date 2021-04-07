501aea7e863dd573ff63a7876aee86bbcf23ddea
gut27 <52551159+gut27@users.noreply.github.com>
2021-04-08 01:44:51 +0900

    Update 2961.py

--- gut27/2961.py
+++ gut27/2961.py
@@ -5,21 +5,23 @@ n = int(input())
 #print(type(n))
 taste = []
 combination = []
-result = 9999
+result = 1000000000
 
 for _ in range(n):
   taste.append(list(map(int, input().split())))
-#.split()있어야 함 taste.append(list(map(int, input().split())))
-#.split()있어야 함
+#.split()있어야 함 
+#taste = [list(map(int, input().split())) for _ in range(n)]
 
+#조합구하기
 for i in range(0,n):
   combination.append(i)
-
 combi_result = []
+
 for i in range(1,n+1):
   for c in combinations(combination,i):
       combi_result.append(c)
 
+#신맛과 쓴맛의 차 구하기
 for combi in combi_result:
   mul = 1
   sum_num = 0
@@ -27,6 +29,24 @@ for combi in combi_result:
     mul = mul * taste[j][0]
     sum_num = sum_num + taste[j][1]
   result = min(result,abs(mul-sum_num))
-#if result ==1
-# break
+
 print(result)
+
+'''
+다른버전
+
+#조합구하기
+combi_result = []
+for i in range(1, n+1):
+  for c in combinations(taste,i):
+      combi_result.append(c)
+
+#신맛과 쓴맛의 차 구하기
+for combi in combi_result:
+  mul = 1
+  sum_num = 0
+  for c in combi:
+    mul *= c[0]
+    sum_num += c[1]
+  result = min(result, abs(mul - sum_num))
+'''
