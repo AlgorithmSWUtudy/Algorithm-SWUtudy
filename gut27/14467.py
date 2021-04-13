1fc9c5517e777c6cea5b2b47dcd664651d42eea5
gut27 <52551159+gut27@users.noreply.github.com>
2021-04-13 16:57:56 +0900

    0413

---
+++ gut27/14467.py
@@ -0,0 +1,14 @@
+n = int(input())
+result = 0
+cow = [9]*11
+print(cow)
+print(type(cow[1]))
+for i in range(n):
+  num, move = map(int, input().split())
+  if cow[num] == 9 :
+    cow[num] = move
+  else:
+    if cow[num] != move:
+      result += 1
+      cow[num] = move
+print(result)
