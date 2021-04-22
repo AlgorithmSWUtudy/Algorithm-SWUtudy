99f429e87e41f1df2d28afada303ecacc901d0e4
Yunhyunjo <gotqlc147@likelion.org>
2021-04-22 14:22:33 +0900

    0408 - AC

--- Yunhyunjo/2667.cpp
+++ Yunhyunjo/2667.cpp
@@ -1,57 +1,76 @@
-// baekjoon 2667 :: WA
+// baekjoon 2667 :: AC - 0ms
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int DFS(int x, int y);
-int find();
+void find();
 
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+vector <string> map;
 int n, x, y;
-vector <string> arr(25);
-
 int main() {
 	
-	ios::sync_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int cnt = 0;
-	
+	ios::sync_with_stdio(false);
+	int cnt, sum = 0;
+	string s;
 	cin >> n;
-
+	vector <int> num;
+	queue <pair <int, int> > q;
+	
 	for(int i = 0; i < n; i++){
-		cin >> arr[i];
+		cin >> s;
+		map.push_back(s);
 	}
 	
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			if(arr[i][j] == '1'){
-				DFS(i, j);
-				cnt++;
+	while(1){
+		find();
+		if(x == n && y == n) break;
+		q.push(make_pair(x,y));
+		map[x][y] = '0';
+		cnt = 1;
+		while(!q.empty()){
+			pair <int, int> tmp = q.front();
+			q.pop();
+			for(int i = 0; i < 4; i++){
+				int a = tmp.first + dx[i];
+				int b = tmp.second + dy[i];
+				if(a >= 0 && a < n && b >= 0 && b <n){
+					if(map[a][b] == '1'){
+						q.push(make_pair(a,b));
+						map[a][b] = '0';
+						cnt++;
+					}
+				}
 			}
 		}
+		sum++;
+		num.push_back(cnt);
 	}
-	
-	cout << cnt;
+	sort(num.begin(), num.end());
+	cout << sum << "\n";
+	for(int i = 0; i < sum; i++){
+		cout << num[i] << "\n";
+	}
 	
 	return 0;
 }
 
-int DFS(int x, int y){
-	int a, b;
-	for(int i = 0; i < 4; i++){
-		a = x + dx[i];
-		b = y + dy[i];
-		if(a >= 0 && a < n && b >= 0 && b <= n){
-			if(arr[a][b] == '1'){
-				arr[a][b] == '0';
-				DFS(a,b);
+void find(){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(map[i][j] == '1'){
+				x = i;
+				y = j;
+				return;
 			}
 		}
 	}
-	return 0;
+	x = n;
+	y = n;
 }
