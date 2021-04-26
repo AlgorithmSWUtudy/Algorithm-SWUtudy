3df8d365a2f259a6eb70d2c3de31a0be73b58afc
gut27 <52551159+gut27@users.noreply.github.com>
2021-04-27 03:29:03 +0900

    Update kakao_blind_2020_2.py

--- gut27/kakao_blind_2020_2.py
+++ gut27/kakao_blind_2020_2.py
@@ -1,35 +1,22 @@
 from itertools import combinations
+from collections import Counter
+
+
 def solution(orders, course):
     answer = []
     array_combi = []
-    array_temp = []
-    array_course = {}
     
-    for order in orders:
-        for i in range(len(order)+1):
+    for c in course:
+        array_combi = []
+        for order in orders:
             order = sorted(order)
-            for combi in combinations(order, i):
-                if len(combi) in course:
-                    array_combi.append("".join(combi))
-    for combi in array_combi:  
-        if len(combi) not in array_course and array_combi.count(combi) >= 2 and len(combi) in course:
-            array_course[len(combi)] = [(combi, array_combi.count(combi))]
-        elif len(combi) in array_course and array_combi.count(combi) >= 2 and len(combi) in course and combi not in array_course[len(combi)]:
-            array_course[len(combi)].append((combi, array_combi.count(combi)))
-
-    for i in course:
-        try:
-            if len(array_course[i]) == 2:
-                answer.append(array_course[i][0])
-            else: 
-                array_temp = list(array_course[i])
-                array_temp.sort(key=lambda x: -x[1])
-                temp = array_temp[0][1]
-                answer.append(array_temp[0][0])
-                for j in range(1, len(array_temp)):
-                    if temp == array_temp[j][1] and array_temp[j][0] not in answer:
-                        answer.append(array_temp[j][0])
-        except:
-            continue
-    answer.sort()
+            combi = list(combinations(order,c))
+            array_combi+=combi
+        count = Counter(array_combi)
+        if not count:
+          return answer
+        temp = max(count.values())
+        for cou in count:
+            if temp == count[cou] and count[cou] >= 2:
+                answer.append("".join(cou))
     return answer
