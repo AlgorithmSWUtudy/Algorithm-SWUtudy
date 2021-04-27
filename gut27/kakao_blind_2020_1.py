e03f09837b0839fd25a123112a55f72fefec7543
gut27 <52551159+gut27@users.noreply.github.com>
2021-04-28 07:44:01 +0900

    Update kakao_blind_2020_1.py

--- gut27/kakao_blind_2020_1.py
+++ gut27/kakao_blind_2020_1.py
@@ -1,47 +1,28 @@
+import re
 def solution(new_id):
     answer = ''
-    check = 0
-    temp = []
-    arrays = ['-','_','.']
-    id = new_id
-    if new_id.islower()==False:
-        check +=1
-        id = id.lower()
-    for i in range(len(id)):
-        if any(array in id[i] for array in arrays) != True:
-            if (id[i].islower() != True) and (id[i].isdigit() != True):
-                temp.append(id[i])
-    for i in range(len(temp)):
-        id = id.replace(temp[i],"")
-        check +=1
-    for i in range(len(id)-1):
-        if id[i:i+2]=='..':
-            id = id.replace('..','.')
-            check +=1
-            i -= 1
-    try:
-        if id[0] == '.':
-            id = id[1:]
-            check +=1
-        if id[-1] == '.':
-            id = id[:-1]
-            check +=1
-    except:
-        pass
-    if len(id) == 0:
-        id = 'a'
-        check +=1
-    if len(id)>=16:
-        id = id[:15]
-        if id[-1] == '.':
-            id = id[:-1]
-        check +=1
-    while len(id) <=2:
-        id = id + id[-1]
-        check +=1
-        
-        
-    if check == 0:
-        return new_id
-    else:
-        return id
+    if re.search('[A-Z]',new_id):
+        new_id = new_id.lower()
+    if re.search('[^a-z0-9-_.]',new_id):
+        new_id = re.sub('[^a-z0-9-_.]', '', new_id)
+    if re.search('[.]{2,}',new_id):
+        new_id = re.sub('[.]{2,}', '.', new_id)
+    if new_id[0] == '.':
+        new_id = new_id[1:]
+    if len(new_id) != 0:
+        if new_id[-1] == '.':
+            new_id = new_id[0:-1]
+    if len(new_id) == 0:
+        new_id = "a"
+    if len(new_id) >= 15:
+        new_id = new_id[0:15]
+        if new_id[-1] == '.':
+            new_id = new_id[0:-1]
+    if len(new_id) <= 2:
+        temp = new_id[-1]
+        for i in range(3):
+            new_id += temp
+            if len(new_id) == 3:
+                break
+    answer = new_id
+    return answer
