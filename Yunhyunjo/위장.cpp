e15635cdc701c2d2d38e99622efdc6799426707e
Yunhyunjo <gotqlc147@likelion.org>
2021-05-13 14:13:26 +0900

    0512

---
+++ Yunhyunjo/위장.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    map <string, int> m;
+    int cnt = 0;
+    
+    for(auto a: clothes){
+        m[a[1]]++;
+    }
+    if(m.size() == 1) return clothes.size();
+    vector <bool> ch(m.size(), false);
+    for(int i = 2; i <= m.size(); i++){
+        for(int j = 0; j < i; j++){
+            ch[j] = true;
+        }
+        do{
+            int b = 0;
+            int c = 1;
+            for(auto a: m){
+                if(ch[b++]){
+                    c *= a.second;
+                }
+            }
+            cnt += c;
+        }while(next_permutation(ch.begin(), ch.end()));
+    }
+    answer = cnt + clothes.size();
+    return answer;
+}
