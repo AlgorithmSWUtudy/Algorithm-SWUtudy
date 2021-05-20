7ea3bc34a3b7c68f4e7e4194fd564777cb593632
gut27 <52551159+gut27@users.noreply.github.com>
2021-05-20 21:31:43 +0900

    Update programmersarray3.py

--- gut27/programmersarray3.py
+++ gut27/programmersarray3.py
@@ -1,12 +1,11 @@
 def solution(citations):
     answer = 0
-    b = 0
     citations.sort(reverse=True)
-    print(citations)
-    for n in range(len(citations)):
-        if citations[n] >= n:
-            b+=1
-    
-    answer = b
 
+    for n in range(len(citations)):
+        if citations[n] >= n+1 and citations[n] != 0:
+            answer+=1
+        else:
+            return answer
+        
     return answer
