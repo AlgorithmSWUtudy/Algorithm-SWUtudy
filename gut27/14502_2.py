508d823d3a43acd00362fd4254775ee7ea0a95c1
gut27 <52551159+gut27@users.noreply.github.com>
2021-06-03 02:01:13 +0900

    0602 -RTE

--- gut27/14502_2.py
+++ gut27/14502_2.py
@@ -1,52 +1,47 @@
-from collections import deque
+# from copy import copy
+
 n, m = map(int,input().split())
 arr= []
 for _ in range(n):
   arr.append(list(map(int,input().split())))
-
+arr_temp = [[0]*m for _ in range(n)]
 dx=[0,0,1,-1]
 dy = [1,-1,0,0]
-
-def safetyzone(arr_new):
+result = 0
+def safetyzone():
   count = 0
   for i in range(n):
     for j in range(m):
-      if arr_new[i][j] == 0:
+      if arr_temp[i][j] == 0:
         count +=1
   return count
 
-def virus(arr_new):
-  queue = deque()
-  for i in range(n):
-    for j in range(m):
-      if arr_new[i][j] == 2:
-        queue.append((i,j))
-  while queue:
-    x,y = queue.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if nx<0 or nx>=n or ny<0 or ny >= m:
-        continue
-      if arr_new[nx][ny] == 1:
-        continue
-      if arr_new[nx][ny] == 0:
-        arr_new[nx][ny] = 2
-        queue.append((nx,ny))
-  return safetyzone(arr_new)
+def virus(x,y):  
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if arr_temp[nx][ny] == 0:
+      arr_temp[nx][ny] = 2
 
 def dfs(count):
-  result = 0
+  global result
+  if count == 3:
+    for i in range(n):
+      for j in range(m):
+        arr_temp[i][j] = arr[i][j]
+    for i in range(n):
+      for j in range(m):
+        if arr[i][j] == 2:
+          virus(i,j)
+    result = max(result,safetyzone())
+    return
   for i in range(n):
     for j in range(m):
       if arr[i][j] == 0:
         count += 1
         arr[i][j] = 1
-      if count < 3:
         dfs(count)
-      if count == 3:
-        result = max(result,virus(arr))
-      count -= 1
-      arr[i][j] = 0
+        count -= 1
+        arr[i][j] = 0
       
 print(dfs(0))
