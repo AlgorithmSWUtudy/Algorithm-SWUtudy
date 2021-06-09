a51ec28f30a95a055c9d1310155a64a0a0392591
gut27 <52551159+gut27@users.noreply.github.com>
2021-06-10 00:14:48 +0900

    0607 :-0609-AC 476ms

--- gut27/17144.py
+++ gut27/17144.py
@@ -1,17 +1,13 @@
-# r,c,t = map(int, input().split())
-# graph = []
-# air_cleaner = []
-# for i in range(r):
-#   a = list(map(int, input().split()))
-#   if -1 in a:
-#     air_cleaner.append((i,0))
-#   graph.append(a)
-r = 7
-c = 8
-t = 6
-graph = [[0, 0, 0, 0, 0, 0, 0, 9], [0, 0, 0, 0, 3, 0, 0, 8], [-1, 0, 5, 0, 0, 0, 22, 0], [-1, 8, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 10, 43, 0], [0, 0, 5, 0, 15, 0, 0, 0], [0, 0, 40, 0, 0, 0, 20, 0]]
-air_cleaner = [(2, 0), (3, 0)]
-print(5//5)
+# PyPy3에서 돌아간다. 파이썬은 시간초과
+
+r,c,t = map(int, input().split())
+graph = []
+air_cleaner = []
+for i in range(r):
+  a = list(map(int, input().split()))
+  if -1 in a:
+    air_cleaner.append((i,0))
+  graph.append(a)
 time = 0
 def air_move(ar,ac):
   dar = [0,-1,0,1]
@@ -26,6 +22,8 @@ def air_move(ar,ac):
       flag += 1
       continue
     if graph[nar][nac] == -1:
+      graph[ar][ac] = temp[0]
+      temp.pop(0)
       break
     temp.append(graph[ar][ac])
     graph[ar][ac] = temp[0]
@@ -40,15 +38,8 @@ def air_move2(ar,ac):
   flag = 0
   ac = ac + 1
   while flag<=4:
-    if time == 1:
-      print('ar')
-      print(ar,ac)
     nar = ar +dar[flag]
     nac = ac + dac[flag] 
-    if time == 1:
-      print('nar')
-      print(nar,nac)
-      print(temp)
     if nar <0 or nar >= r or nac<0 or nac >= c:
       flag += 1
       continue
@@ -82,11 +73,8 @@ while time < t:
   for i in range(r):
     for j in range(c):
       graph[i][j] = graph[i][j] + temp_graph[i][j]
-  print('hono1',graph)
   air_move(air_cleaner[0][0],air_cleaner[0][1])
-  print('hono2',graph)
   air_move2(air_cleaner[1][0],air_cleaner[1][1])
-  print('hono3',graph)
   time += 1
 
 result = 0
