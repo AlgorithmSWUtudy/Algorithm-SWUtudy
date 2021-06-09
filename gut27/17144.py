12eeb8ee62f7177c1ae7d2dfc225f1e4b6986de2
gut27 <52551159+gut27@users.noreply.github.com>
2021-06-09 23:59:18 +0900

    Update 17144.py

--- gut27/17144.py
+++ gut27/17144.py
@@ -1,75 +1,61 @@
-#다시풀기
-r,c,t = map(int, input().split())
-graph = []
-arr_cleaner = []
-for i in range(r):
-  a = list(map(int, input().split()))
-  if -1 in a:
-    air_cleaner = [(i,0),(i+1,0)]
-  graph.append(a)
+# r,c,t = map(int, input().split())
+# graph = []
+# air_cleaner = []
+# for i in range(r):
+#   a = list(map(int, input().split()))
+#   if -1 in a:
+#     air_cleaner.append((i,0))
+#   graph.append(a)
+r = 7
+c = 8
+t = 3
+graph = [[0, 0, 0, 0, 0, 0, 0, 9], [0, 0, 0, 0, 3, 0, 0, 8], [-1, 0, 5, 0, 0, 0, 22, 0], [-1, 8, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 10, 43, 0], [0, 0, 5, 0, 15, 0, 0, 0], [0, 0, 40, 0, 0, 0, 20, 0]]
+air_cleaner = [(2, 0), (3, 0)]
+print(5//5)
 time = 0
-
-def air_moev(ar,ac):
-  temp_graph = [[0]*c for _ in range(r)]
+def air_move(ar,ac):
   dar = [0,-1,0,1]
   dac = [1,0,-1,0] 
+  temp = [0]
   flag = 0
-  ar2= ar
-  ac2 = ac
   ac = ac + 1
-  while flag<4:
+  while flag<=4:
     nar = ar +dar[flag]
     nac = ac + dac[flag] 
-    if nar <0 or nar >= r or nac<0 or nar >= c:
+    if nar <0 or nar >= r or nac<0 or nac >= c:
       flag += 1
       continue
     if graph[nar][nac] == -1:
       break
-    temp_graph[nar][nac] = graph[ar][ac]
+    temp.append(graph[ar][ac])
+    graph[ar][ac] = temp[0]
+    temp.pop(0)
     ar = nar
     ac = nac 
-  while flag<4:
-    nar = ar2 +dar[flag]
-    nac = ac2 + dac[flag] 
-    if nar <0 or nar >= r or nac<0 or nar >= c:
-      flag += 1
-      continue
-    graph[nar][nac] = graph[nar][nac]
-    if graph[nar][nac] == -1:
-      break
-    ar = nar
-    ac = nac
-  return temp_graph
-def air_moev2(ar,ac):
-  dar = [0,-1,0,1]
+
+def air_move2(ar,ac):
+  dar = [0,1,0,-1]
   dac = [1,0,-1,0]
-  temp_graph = [[0]*c for _ in range(r)]
+  temp = [0]
   flag = 0
-  ac2 = ac
   ac = ac + 1
-  ar2 = ar
-  while flag<4:
+  while flag<=4:
+    print('ar')
+    print(ar,ac)
     nar = ar +dar[flag]
     nac = ac + dac[flag] 
-    if nar <0 or nar >= r or nac<0 or nar >= c:
+    print('')
+    print(nar,nac)
+    if nar <0 or nar >= r or nac<0 or nac >= c:
       flag += 1
       continue
     if graph[nar][nac] == -1:
       break
-    temp_graph[nar][nac] = graph[ar][ac]
+    temp.append(graph[ar][ac])
+    graph[ar][ac] = temp[0]
+    temp.pop(0)
     ar = nar
     ac = nac 
-  while flag<4:
-    nar = ar2 +dar[flag]
-    nac = ac2 + dac[flag] 
-    if nar <0 or nar >= r or nac<0 or nar >= c:
-      flag += 1
-      continue
-    graph[nar][nac] = graph[nar][nac]
-    if graph[nar][nac] == -1:
-      break
-    ar = nar
-    ac = nac
 
 di = [0,0,-1,1]
 dj = [1,-1,0,0]
@@ -83,29 +69,26 @@ while time < t:
       for z in range(4):
         ni = i + di[z]
         nj = j + dj[z]
-        if ni < 0 or ni >= r or nj <0 or nj >= c:
+        if ni < 0 or ni >= r or nj <0 or nj >= c or graph[ni][nj] == -1:
           continue
-        temp_graph[ni][nj] = graph[i][j]//5
+        temp_graph[ni][nj] = temp_graph[ni][nj] + graph[i][j]//5
         count+=1
+      if (time == 1):
       graph[i][j] = graph[i][j] - ((graph[i][j]//5)*count)
-  print(graph,temp_graph)
   for i in range(r):
     for j in range(c):
       graph[i][j] = graph[i][j] + temp_graph[i][j]
-  print('honono')
-  print(graph)
-
-  down_ar = air_cleaner[1][0]
-  down_ac = air_cleaner[1][1]
-  air_moev(air_cleaner[0][0],air_cleaner[0][1])
-  air_moev2(air_cleaner[0][0],air_cleaner[0][1])
+  print('hono1',graph)
+  air_move(air_cleaner[0][0],air_cleaner[0][1])
+  print('hono2',graph)
+  air_move2(air_cleaner[1][0],air_cleaner[1][1])
+  print('hono3',graph)
   time += 1
-  print('time',time)
-  print(graph)
+
 result = 0
-for i in range(i):
-  for j in range(j):
-    if graph[i][j] == -1:
+for i in range(r):
+  for j in range(c):
+    if graph[i][j] == -1 or graph[i][j] == 0:
       continue
     result += graph[i][j]
 
