6bafdcd248bf5a35732e470ed89c491ddb6c62a1
Yunhyunjo <gotqlc147@likelion.org>
2021-06-10 17:35:08 +0900

    0610 - AC 3ms

--- Yunhyunjo/길찾기게임.cpp
+++ Yunhyunjo/길찾기게임.cpp
@@ -6,32 +6,59 @@
 using namespace std;
 int grid[10000][10000];
 
+struct Btree{
+    int data;
+    Btree *left = NULL, *right = NULL;
+};
+
+Btree *makeTree(vector<vector<int>> &nodeinfo, int start, int end){
+    if(start > end) return NULL;
+    int max = nodeinfo[start][1];
+    int idx = start;
+    for(int i = start + 1; i <= end; i++){
+        if(max < nodeinfo[i][1]){
+            idx = i;
+            max = nodeinfo[i][1];
+        }
+    }
+    Btree *tree = new Btree();
+    tree->data = nodeinfo[idx][2];
+    tree->left = makeTree(nodeinfo, start, idx-1);
+    tree->right = makeTree(nodeinfo, idx+1, end);
+    return tree;
+}
+
+void pre(Btree *tree, vector <int> &v){
+    if(tree == NULL) {
+        return;
+    }
+    v.push_back(tree->data);
+    pre(tree->left,v);
+    pre(tree->right,v);
+}
+
+void pos(Btree *tree, vector <int> &v){
+    if(tree == NULL) {
+        return;
+    }
+    pos(tree->left, v);
+    pos(tree->right, v);
+    v.push_back(tree->data);
+}
+
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
     vector<vector<int>> answer;
     int num = 1;
     int mx = 0, my = 0;
-    for(auto a: nodeinfo){
-        grid[a[0]][a[1]] = num++;
-        mx = max(mx, a[0]);
-        my = max(my, a[1]);
+    for(int i = 0; i < nodeinfo.size(); i++){
+        nodeinfo[i].push_back(num++);
     }
-    vector <vector <int>> tree;
-    for(int i = my; i >= 0; i--){
-        vector <int> a;
-        for(int j = 0; j <= mx; j++){
-            if(grid[j][i] != 0){
-                a.push_back(grid[j][i]);
-            }
-        }
-        if(!a.empty()) tree.push_back(a);
-    }
-    
-    for(auto a: tree){
-        for(int i: a){
-            cout << i << " ";
-        }
-        cout << endl;
-    }
-    
+    sort(nodeinfo.begin(), nodeinfo.end());
+    Btree *tree = makeTree(nodeinfo, 0, nodeinfo.size()-1);
+    vector <int> pr, po;
+    pre(tree, pr);
+    pos(tree, po);
+    answer.push_back(pr);
+    answer.push_back(po);
     return answer;
 }
