0c54356de44c833bd31de1c41d6b99c6dd95b858
Yunhyunjo <gotqlc147@likelion.org>
2021-06-16 22:17:26 +0900

    0601 : 0616 - AC 0ms

--- Yunhyunjo/3190.cpp
+++ Yunhyunjo/3190.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 int map[101][101];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
 
 int main() {
 
@@ -26,41 +29,78 @@ int main() {
 		cin >> v[i].first >> v[i].second;
 	}
 
-	int dx = 0;
-	int dy = 1;
+	int dir = 0;
 	int idx = 0;
-	int hx = 1, hy = 1, tx = 1, ty = 1;
-	bool flag;
+	int hx, hy;
+	deque < pair <int, int>> snake;
+	snake.push_back(make_pair(1, 1));
+	bool flag = false;
 	int t = 0;
 	while (1) {
-		for (int i = 0; i < v[idx].first; i++) {
-			if (hx + dx > 0 && hx + dx <= n && hy + dy > 0 && hy + dy <= n && map[hx + dx][hy + dy] != 2) {
-				t++;
-				if (map[hx + dx][hy + dy] == 1) {
-					hx += dx;
-					hy += dy;
-					map[hx + dx][hy + dy] = 2;
+		if (idx >= L) {
+			hx = snake.back().first;
+			hy = snake.back().second;
+			t++;
+			if (hx + dx[dir] > 0 && hx + dx[dir] <= n && hy + dy[dir] > 0 && hy + dy[dir] <= n && map[hx + dx[dir]][hy + dy[dir]] != 2) {
+				if (map[hx + dx[dir]][hy + dy[dir]] == 1) {
+					snake.push_back(make_pair(hx + dx[dir], hy + dy[dir]));
+					map[hx + dx[dir]][hy + dy[dir]] = 2;
 				}
 				else {
-					hx += dx;
-					hy += dy;
+					snake.push_back(make_pair(hx + dx[dir], hy + dy[dir]));
+					int a = snake.front().first;
+					int b = snake.front().second;
+					map[a][b] = 0;
+					map[hx + dx[dir]][hy + dy[dir]] = 2;
+					snake.pop_front();
+				}
+			}
+			else break;
+			continue;
+		}
+		int num = v[idx].first - t;
+		for (int i = 0; i < num; i++) {
+			hx = snake.back().first;
+			hy = snake.back().second;
+			t++;
+			if (hx + dx[dir] > 0 && hx + dx[dir] <= n && hy + dy[dir] > 0 && hy + dy[dir] <= n && map[hx + dx[dir]][hy + dy[dir]] != 2) {
+				if (map[hx + dx[dir]][hy + dy[dir]] == 1) {
+					snake.push_back(make_pair(hx + dx[dir], hy + dy[dir]));
+					map[hx + dx[dir]][hy + dy[dir]] = 2;
+				}
+				else {
+					snake.push_back(make_pair(hx + dx[dir], hy + dy[dir]));
+					int a = snake.front().first;
+					int b = snake.front().second;
+					map[a][b] = 0;
+					map[hx + dx[dir]][hy + dy[dir]] = 2;
+					snake.pop_front();
 				}
 			}
 			else {
-				break;
 				flag = true;
+				break;
 			}
 		}
 		if (flag) break;
-		if (v[idx].second == 'L') {
-			dx = -dx;
-			dy = -dy;
+		if (dir == 0) {
+			if (v[idx].second == 'L') dir = 3;
+			else dir = 1;
+		}
+		else if (dir == 1) {
+			if (v[idx].second == 'L') dir = 0;
+			else dir = 2;
+		}
+		else if (dir == 2) {
+			if (v[idx].second == 'L') dir = 1;
+			else dir = 3;
 		}
 		else {
-			int tmp = dx;
-			dx = dy;
-			dy = tmp;
+			if (v[idx].second == 'L') dir = 2;
+			else dir = 0;
 		}
+		
+		idx++;
 	}
 
 	cout << t;
