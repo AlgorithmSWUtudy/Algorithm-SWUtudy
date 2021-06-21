64bc66df8ac82c3d9e9e7f1ca2716c0e6d290538
gut27 <52551159+gut27@users.noreply.github.com>
2021-06-22 01:54:43 +0900

    0621

--- gut27/17413.py
+++ gut27/17413.py
@@ -1,16 +1,32 @@
-S = 'noojkeab enilno egduj'
-S = S.split('>')
+S = 'baekjoon online judge'
+s = 0
+temp = []
 for i in range(len(S)):
-  if '>' in S[i]:
-    S[i] = S[i].split('>')
-print(S)
-for ar in arr:
-  temp = ''
-  if '<' in ar:
+  if '<' == S[i]:
+    temp.append(i)
+  if S[i] == '>':
+    temp.append(i)
+
+arr = []
+if len(temp) == 0:
+  arr.append(S)
+elif temp[0] > 0:
+  arr.append(S[0:temp[0]])
+else:
+  for i in range(len(temp)-1):
+    if i % 2 == 0:
+      arr.append(S[temp[i]:(temp[i+1]+1)])
+    else:
+      arr.append(S[(temp[i]+1):temp[i+1]])
+if len(temp) > 0 and temp[-1] != (len(S)-1):
+  arr.append(S[(temp[-1]+1):])
+
+for a in range(len(arr)):
+  if '<' in arr[a]:
     continue
-  ar = ar.split(' ')
-  for a in ar:
-    temp += a.reverse
-  ar = temp
+  temp = arr[a].split(' ')
+  for i in range(len(temp)):
+    temp[i] = temp[i][::-1]
+  arr[a] = ' '.join(temp)
 
 print(''.join(arr))
