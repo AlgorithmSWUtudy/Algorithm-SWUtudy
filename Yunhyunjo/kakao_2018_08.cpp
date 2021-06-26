0bcf52158eca9eb1860434af1dc067a87e4d6ee8
Yunhyunjo <gotqlc147@likelion.org>
2021-06-26 22:09:30 +0900

    0624 : 0626 - AC 0.3ms

--- Yunhyunjo/kakao_2018_08.cpp
+++ Yunhyunjo/kakao_2018_08.cpp
@@ -17,30 +17,64 @@ vector <string> split(string s, char delim){
     return v;
 }
 
+string make(string s){
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '#') {
+            if(s[i-1] == 'C') s.replace(i-1,2,"c");
+            else if(s[i-1] == 'A') s.replace(i-1,2,"a");
+            else if(s[i-1] == 'D') s.replace(i-1,2,"d");
+            else if(s[i-1] == 'E') s.replace(i-1,2,"e");
+            else s.replace(i-1,2,"g");
+        }
+    }
+    return s;
+}
+
+struct Music{
+    int n, idx;
+    string s;
+    Music(int n, int idx, string s){
+        this->n = n;
+        this->idx = idx;
+        this->s = s;
+    }
+    bool operator <(const Music &a)const{
+        if(n != a.n) return n > a.n;
+        return idx < a.idx;
+    }
+};
+
 string solution(string m, vector<string> musicinfos) {
     string answer = "";
-    vector <pair <int, string>> res;
-    // map <string, int> m = {"C" : 1, "C#" : 2, "D" : 3, "D#" : 4, "E" : 5, "F" : 6, "F#" : 7, "G" : 8, "G#" : 9,  "A" : 10, "A#" : 11, "B" : 12, "E#" : 13};
+    vector <Music> res;
+    m = make(m);
+    int i = 1;
+    cout << m << endl;
     for(auto a: musicinfos){
         vector <string> v = split(a, ',');
         int s, e;
         s = stoi(v[0].substr(0, 2)) * 60 + stoi(v[0].substr(3, 2));
         e = stoi(v[1].substr(0, 2)) * 60 + stoi(v[1].substr(3, 2));
-        s = e - s;
+        if(s > e) s = 1440 - s;
+        else s = e - s;
         int cnt = 0;
-        for(char c: v[3]){
-            if(c == '#') continue;
-            else cnt++;
+        v[3] = make(v[3]);
+        if(s < v[3].size()){
+             string ss = v[3].substr(0,s);
+             if(ss.find(m) != std::string::npos){
+                 res.push_back(Music(s, i++, v[2]));
+             }
+            continue;
         }
-        e = s / cnt;
-        for(int i = 0; i < e; i++){
+        while(1){
             v[3] += v[3];
+            if(m.size() < v[3].size()) break;
         }
         if(v[3].find(m) != std::string::npos){
-            res.push_back(make_pair(s, v[2]));
+            res.push_back(Music(s, i++, v[2]));
         }
     }
-    if(res.empty()) return "none";
+    if(res.empty()) return "(None)";
     sort(res.begin(), res.end());
-    return res[0].second;
+    return res[0].s;
 }
