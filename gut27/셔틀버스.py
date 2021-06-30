b8bc3e0603f1d1f90a20d67762fc141cd6ddcf97
gut27 <52551159+gut27@users.noreply.github.com>
2021-06-30 23:10:27 +0900

    0630

--- gut27/셔틀버스.py
+++ gut27/셔틀버스.py
@@ -1,5 +1,64 @@
 def solution(n, t, m, timetable):
     answer = ''
+    for ti in range(len(timetable)):
+        temp = timetable[ti].split(':')
+        timetable[ti] = int(temp[0])*60 + int(temp[1])
+    arr = []
+    
+    end = (n-1) *t
+    for i in range(0,end+1,t):
+        temp = 9*60 + i
+        arr.append(temp)
+        
+    for i in range(0,len(arr)-1):
+        count = m
+        for j in range(len(timetable)):
+            if count == 0:
+                break
+            if timetable[j] != -1 and arr[i]>timetable[j]:
+                count -= 1
+                timetable[j] = -1
+    table = []
+    for t in timetable:
+        if t != -1:
+            table.append(t)
+    
+    count = 0
+    table.sort()
+    for t in table:
+        if arr[-1] >= t and t != -1:
+            count += 1
+    if count < m:
+        r,q = divmod(arr[-1], 60)
+        return str(r).zfill(2)+':'+str(q).zfill(2)
+    else:
+        point = table[0]
+        count = m
+        before = table[0]
+        for t in table:
+            if arr[-1] >= t:
+                count -= 1
+                if count >0:
+                    if t == before:
+                        continue
+                    else:
+                        point = t
+                        before = t
+                else:
+                    break
+    if point == 0:
+        r,q = divmod(table[i]-1, 60)
+        return str(r).zfill(2)+':'+str(q).zfill(2)
+    else:
+        r,q = divmod(point, 60)
+        return str(r).zfill(2)+':'+str(q).zfill(2)
+        
+    return answer
+    
+solution(2,1,2,["09:00", "09:00", "09:00", "09:00"])
+
+def solution(n, t, m, timetable):
+    answer = ''
     # 값을 int로 바꿔서 계산 편의 도모
     for t in range(len(timetable)):
         temp = timetable[t].split(':')
