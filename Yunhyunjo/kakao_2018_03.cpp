9515f17843443e1efff2b94627923c0853c38f49
Yunhyunjo <gotqlc147@likelion.org>
2021-07-05 22:56:56 +0900

    0628 : 0705 - AC 0.05ms

--- Yunhyunjo/kakao_2018_03.cpp
+++ Yunhyunjo/kakao_2018_03.cpp
@@ -1,39 +1,34 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 string solution(int n, int t, int m, vector<string> timetable) {
     string answer = "";
     vector <int> v;
+    sort(timetable.begin(), timetable.end());
     for(auto a: timetable){
         v.push_back(stoi(a.substr(0,2))*60 + stoi(a.substr(3)));
     }
-    sort(v.begin(), v.end());
     int j = 0, h, mi;
     int s = 540;
-    while(n--){
+    while(n != 1){
         for(int i = 0; i < m; i++){
-            if(timetable.size() == j) {
-                h = s / 60;
-                mi = s % 60;
-                if(h < 10) answer += '0';
-                answer += to_string(h);
-                if(mi < 10) answer += ":0";
-                else answer += ':';
-                answer += to_string(mi);
-                return answer;
-            }
+            if(timetable.size() == j) break;
             if(s >= v[j]) j++;
             else break;
         }
         s += t;
+        n--;
+    }        
+    for(int i = 0; i < m-1; i++){
+        if(j == v.size()) break;
+        if(s >= v[j]) j++;
     }
-    if(j == v.size())  s = v.back()-1;
-    else if(j == 0) s -= t;
-    else if (j < v.size()) s = v[j-1] -1;
+    if(j != v.size()) {
+        if(s > v[j] - 1) s = v[j] - 1;
+    }
     h = s / 60;
     mi = s % 60;
     if(h < 10) answer += '0';
