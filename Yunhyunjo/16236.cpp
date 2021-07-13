30e3522944dd329644f7ed6365908e2add91c9b1
Yunhyunjo <gotqlc147@likelion.org>
2021-07-13 23:46:42 +0900

    0519 : 0527 - AC 0ms 0713 - WA

--- Yunhyunjo/16236.cpp
+++ Yunhyunjo/16236.cpp
@@ -3,94 +3,73 @@
 #include <queue>
 
 using namespace std;
+int map[20][20];
+int ch[20][20];
+struct Shark {
+	int s, c, t;
+	void sizeUp() {
+		s++;
+		c = 0;
+	}
+};
 
-int memo[20][20];
-
-struct State {
-	int x, y, dis;
-	State(int x, int y, int dis) {
+struct Point {
+	int x, y, t;
+	Point(int x, int y, int t) {
 		this->x = x;
 		this->y = y;
-		this->dis = dis;
-	}
-
-	bool operator <(const State& a)const {
-		if (dis == a.dis) {
-			if (x == a.x) return y > a.y;
-			else return x > a.x;
-		}
-		else return dis > a.dis;
+		this->t = t;
 	}
 };
-
-struct Shark {
-	int x, y, ate = 0, size = 2;
-	void sizeUp() {
-		size++;
-		ate = 0;
-	}
-};
-
 int main() {
 
 	ios_base::sync_with_stdio(false);
 
 	int n, res = 0;
-	Shark shark;
 
 	cin >> n;
-
-	vector <vector <int>> map;
+	int a, b;
 	for (int i = 0; i < n; i++) {
-		vector <int> a(n);
 		for (int j = 0; j < n; j++) {
-			cin >> a[j];
-			if (a[j] == 9) {
-				shark.x = i;
-				shark.y = j;
-				a[j] = 0;
+			cin >> map[i][j];
+			if (map[i][j] == 9) {
+				a = i;
+				b = j;
 			}
 		}
-		map.push_back(a);
 	}
-
-	int dx[] = { -1, 0 , 0, 1 };
+	Shark sh;
+	sh.s = 2;
+	sh.c = 0;
+	sh.t = 0;
+	int dx[] = { -1, 0, 0, 1 };
 	int dy[] = { 0, -1, 1, 0 };
-	int ch[20][20] = { 0, };
-
-	priority_queue <State> pq;
-	pq.push(State(shark.x, shark.y, 0));
-
-	while (!pq.empty()) {
-		State tmp = pq.top();
-		pq.pop();
-		int x = tmp.x;
-		int y = tmp.y;
-		int z = tmp.dis;
-		if (map[x][y] != 0 && map[x][y] < shark.size) {
-			shark.x = x;
-			shark.y = y;
-			shark.ate++;
-			if (shark.ate >= shark.size) shark.sizeUp();
-			map[x][y] = 0;
+	queue <Point> q;
+	q.push(Point(a, b, 0));
+	while (!q.empty()){
+		Point p = q.front();
+		q.pop();
+		if (map[p.x][p.y] != 0 && map[p.x][p.y] < sh.s) {
+			sh.c++;
+			map[p.x][p.y] = 0;
+			if (sh.c == sh.s) sh.sizeUp();
+			while (!q.empty()) q.pop();
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
 					ch[i][j] = 0;
 				}
 			}
-			while (!pq.empty()) pq.pop();
-			res = z;
+			sh.t = p.t;
 		}
 		for (int i = 0; i < 4; i++) {
-			int xx = x + dx[i];
-			int yy = y + dy[i];
-			if (xx < 0 || xx >= n || yy < 0 || yy >= n || map[xx][yy] > shark.size || ch[xx][yy] > 0) continue;
-			pq.push(State(xx, yy, z + 1));
-			ch[xx][yy] = 1;
+			if (p.x + dx[i] >= 0 && p.x + dx[i] < n && p.y + dy[i] >= 0 && p.y + dy[i] < n && ch[p.x + dx[i]][p.y + dy[i]] == 0) {
+				q.push(Point(p.x + dx[i], p.y + dy[i], p.t + 1));
+				ch[p.x + dx[i]][p.y + dy[i]] = 1;
+			}
 		}
 	}
 
-	cout << res;
+	cout << sh.t;
 
 	return 0;
 }
