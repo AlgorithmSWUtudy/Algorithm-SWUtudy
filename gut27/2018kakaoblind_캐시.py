e7d45512aeac0ae856ba5469290ac1000d500975
gut27 <52551159+gut27@users.noreply.github.com>
2021-08-03 20:53:49 +0900

    Create 2018kakaoblind_캐시.py

---
+++ gut27/2018kakaoblind_캐시.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    queue = deque(maxlen = cacheSize)
+    answer = 0
+    for city in cities:
+        city = city.lower()
+        if city in queue:
+            answer += 1
+            queue.remove(city)
+            queue.append(city)
+        else:
+            queue.append(city)
+            answer += 5
+    return answer
+//append-  오른쪽 삽입, popleft 왼쪽에서 빼기
