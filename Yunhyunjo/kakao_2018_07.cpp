00eb575ff6c0ea4727c791e9fc0d56f5a24ab4ba
Yunhyunjo <gotqlc147@likelion.org>
2021-08-04 19:28:04 +0900

    0804 - AC 0.01ms

--- Yunhyunjo/kakao_2018_07.cpp
+++ Yunhyunjo/kakao_2018_07.cpp
@@ -1,52 +1,39 @@
 #include <string>
-#include <vector>
-#include <iostream>
 
 using namespace std;
 
 int solution(string dartResult) {
-    int answer = 0;
-    vector <int> s;
-    int sc;
-    char p = ' ';
+    int score[3];
+    int n = 0;
+    char c = ' ';
     for(int i = 0; i < dartResult.size(); i++){
-        if(dartResult[i] > 47 && dartResult[i] < 58){
-            if(dartResult[i+1] == 48) {
-                sc = 10;
-                i++;
-            }
-            else sc = dartResult[i] - 48;
+        if(dartResult[i] == '*'){
+            if(n != 0) score[n-1] = score[n-1] * 2;
+            score[n] = score[n] * 2;
+            c = '*';
+            n++;
         }
-        else if(dartResult[i] > 64 && dartResult[i] < 91){
-            if(dartResult[i] == 'S') s.push_back(sc);
-            else if(dartResult[i] == 'D') s.push_back(sc * sc);
-            else s.push_back(sc*sc*sc);
+        else if(dartResult[i] == '#'){
+            score[n] = -score[n] ;
+            c = '#';
+            n++;
         }
         else{
-            if(dartResult[i] == '*'){
-                if(p == '*'){
-                    s[s.size()-2] *= 4;
-                    s[s.size()-1] *= 2;
-                }
-                else if(p == '#'){
-                    s[s.size()-2] *= 2;
-                    s[s.size()-1] *= 2;
-                    p = '#';
-                }
-                else {
-                    s[s.size()-2] *= 2;
-                    s[s.size()-1] *= 2;
-                    p = ' ';
-                }
+            int num = i+1;
+            score[n] = dartResult[i] - 48;
+            if(dartResult[num] == '0'){
+                num++;
+                score[n] = 10;
+                i++;
             }
-            else{
-                s[s.size()-1] *= (-1);
-                p = '#';
+            if(dartResult[num] == 'D') score[n] *= score[n]; 
+            else if(dartResult[num] == 'T') score[n] *= score[n] * score[n];
+            if(num+1 < dartResult.size() && dartResult[num+1] > 47 && dartResult[num+1] < 58) {
+                c = ' ';
+                n++;
             }
+            i++;
         }
     }
-    for(auto a: s){
-        answer += a;
-    }
-    return answer;
+    return score[0] + score[1] + score[2];
 }
