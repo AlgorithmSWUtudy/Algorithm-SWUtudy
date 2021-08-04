0790937fe14736380dc87fe3c24fbd785048511d
Yunhyunjo <gotqlc147@likelion.org>
2021-08-04 20:58:56 +0900

    0804 - AC 3.4ms

--- Yunhyunjo/kakao_2018_05.cpp
+++ Yunhyunjo/kakao_2018_05.cpp
@@ -1,36 +1,41 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
-    map <string, int> m;
-    if(cacheSize == 0) return 5*cities.size();
+    map <string, int> cache;
+    vector <pair <int, string>> tmp;
+    
+    if(cacheSize == 0) return cities.size()*5;
+    
     for(int i = 0; i < cities.size(); i++){
         transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::toupper);
-        if(m[cities[i]] > 0){
-            m[cities[i]] = i+1;
-            answer++;
+        if(cache[cities[i]] == 0){
+            if(tmp.size() < cacheSize) tmp.push_back(make_pair(i+1, cities[i]));
+            else{
+                sort(tmp.begin(), tmp.end());
+                cache[tmp[0].second] = 0;
+                tmp[0].second = cities[i];
+                tmp[0].first = i+1;
+            }
+            cache[cities[i]] = i+1;
+            answer += 5;
         }
         else{
-            answer += 5;
-            if(m.size() > cacheSize){
-                vector <pair <int, string>> v;
-                for(auto it = m.begin();  it!= m.end(); it++){
-                    v.push_back(make_pair(it->second, it->first));
+            for(int j = 0; j < cacheSize; j++){
+                if(tmp[j].second == cities[i]){
+                    tmp[j].first = i+1;
+                    break;
                 }
-                sort(v.begin(), v.end());
-                m.erase(v[1].second);
-                m[cities[i]] = i+1;
             }
-            else{
-                m[cities[i]] = i+1;
-            }
+            cache[cities[i]] = i+1;
+            answer++;
         }
     }
-    
+
     return answer;
 }
