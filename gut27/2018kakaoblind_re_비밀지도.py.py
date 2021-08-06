946d333d0abf58dd46188f0007b9e8df4d91a2fd
gut27 <52551159+gut27@users.noreply.github.com>
2021-08-07 02:12:29 +0900

    0806 -AC

--- gut27/2018kakaoblind_re_비밀지도.py.py
+++ gut27/2018kakaoblind_re_비밀지도.py.py
@@ -1,5 +1,4 @@
-# 이진수 만드는 함수
-def change(num,n):
+def binary(num,n):
     q = 0
     temp = ''
     for _ in range(n):
@@ -11,22 +10,14 @@ def change(num,n):
 
 def solution(n, arr1, arr2):
     answer = []
-    arr_sum = [[0]*n for _ in range(n)]
-
-    # 복호화
     for i in range(n):
-        arr1[i] = change(arr1[i],n)
-        arr2[i] = change(arr2[i],n)
-
-    for i in range(n):
-        for j in range(n):
-            # 모두 공백일 경우 '' 
-            if arr1[i][j] == '0' and arr2[i][j] == '0':
-                arr_sum[i][j] = ' '
-            else:
-                arr_sum[i][j] = '#'
-        answer.append(''.join(arr_sum[i]))
-        
+        a = binary(arr1[i],n)
+        b = binary(arr2[i],n)
+        temp_str = ''
+        for j in range(len(a)):
+            if a[j] == '0'and b[j] == '0':
+                temp_str+= ' '
+            else: 
+                temp_str += '#'
+        answer.append(temp_str)
     return answer
-
-# bin으로 하려다가 실패함 0xb때문에 어떻게 해야하는지 찾아보기 문자로 바꾸는 방법
