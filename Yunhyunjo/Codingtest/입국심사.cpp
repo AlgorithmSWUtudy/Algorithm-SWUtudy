2e7c5fc6a0060bf61ff348c3b7ea4e5e6565dc9b
Yunhyunjo <gotqlc147@likelion.org>
2021-08-13 21:06:59 +0900

    0813 Codingtest - AC 23.47ms

--- Yunhyunjo/Codingtest/입국심사.cpp
+++ Yunhyunjo/Codingtest/입국심사.cpp
@@ -8,20 +8,21 @@ long long solution(int n, vector<int> times) {
     long long answer = 0;
     
     sort(times.begin(), times.end());
-    long long lt = 1, rt = times[times.size() -1] * (long long)n, mid;
+    
+    long long lt = 1, rt = (long long)n * times[0], mid, num;
     
     while(lt <= rt){
         mid = (lt + rt) / 2;
-        long long num = 0;
+        num = 0;
         for(int i: times){
-            num = num + (mid / i);
+            num += (mid / i);
         }
+        
         if(num >= n) {
             rt = mid - 1;
             answer = mid;
         }
         else lt = mid + 1;
     }
-    
     return answer;
 }
