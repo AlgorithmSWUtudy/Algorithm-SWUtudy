31b871f97c0788b78fb84748e8434982d26a4e0b
Yunhyunjo <gotqlc147@likelion.org>
2021-08-14 15:27:37 +0900

    0814 Codingtest - AC 1.38ms

--- Yunhyunjo/Codingtest/kakao_2018_05.cpp
+++ Yunhyunjo/Codingtest/kakao_2018_05.cpp
@@ -1,41 +1,30 @@
 #include <string>
-#include <vector>
+#include <deque>
 #include <algorithm>
-#include <map>
 
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
+    
     int answer = 0;
-    map <string, int> cache;
-    vector <pair <int, string>> tmp;
+    if(cacheSize == 0) return 5 * cities.size();
     
-    if(cacheSize == 0) return cities.size()*5;
+    deque <string> cache;
     
-    for(int i = 0; i < cities.size(); i++){
-        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::toupper);
-        if(cache[cities[i]] == 0){
-            if(tmp.size() < cacheSize) tmp.push_back(make_pair(i+1, cities[i]));
-            else{
-                sort(tmp.begin(), tmp.end());
-                cache[tmp[0].second] = 0;
-                tmp[0].second = cities[i];
-                tmp[0].first = i+1;
-            }
-            cache[cities[i]] = i+1;
-            answer += 5;
-        }
-        else{
-            for(int j = 0; j < cacheSize; j++){
-                if(tmp[j].second == cities[i]){
-                    tmp[j].first = i+1;
-                    break;
-                }
-            }
-            cache[cities[i]] = i+1;
+    for(string s: cities){
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        deque <string> ::iterator it = find(cache.begin(), cache.end(), s);
+        if(it != cache.end()){
+            cache.erase(it);
+            cache.push_back(s);
             answer++;
         }
+        else{
+            if(cache.size() >= cacheSize) cache.pop_front();
+            cache.push_back(s);
+            answer += 5;
+        }
     }
-
+    
     return answer;
 }
