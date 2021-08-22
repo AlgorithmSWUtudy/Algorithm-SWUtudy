5b10d3ff8e735757a006f41f1ab9a985549532ba
Yunhyunjo <gotqlc147@likelion.org>
2021-08-22 21:31:46 +0900

    0822 Codingtest - AC 0.04ms

--- Yunhyunjo/Codingtest/Programmers/kakao_2018_02.cpp
+++ Yunhyunjo/Codingtest/Programmers/kakao_2018_02.cpp
@@ -1,48 +1,53 @@
 #include <string>
-#include <algorithm>
 #include <map>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-map <string, int> make(string s){
-    map <string, int> m;
-    transform(s.begin(), s.end(), s.begin(), ::toupper);
+map <string, int> create(string s){
+    map <string, int> res;
     for(int i = 0; i < s.size()-1; i++){
-        if(s[i] < 65 || s[i] > 91) continue;
-        if(s[i+1] < 65 || s[i+1] > 91){
-            i++;
+        if(s[i] < 65 || s[i] > 90){
             continue;
         }
-        string str = s.substr(i,2);
-        m[str]++;
+        if(s[i+1] < 65 || s[i+1] > 91){
+            i++; continue;
+        }
+        string str = "";
+        str += s[i];
+        str += s[i+1];
+        res[str]++;
     }
-    return m;
+    return res;
 }
 
 int solution(string str1, string str2) {
     int answer = 0;
-    map <string, int> m1 = make(str1);
-    map <string, int> m2 = make(str2);
-    map <string, int>::iterator it;
+    transform(str1.begin(), str1.end(), str1.begin(), ::toupper);
+    transform(str2.begin(), str2.end(), str2.begin(), ::toupper);
+    map <string, int> m1 = create(str1);
+    map <string, int> m2 = create(str2);
+    int r = 0, a = 0;
     if(m1.empty() && m2.empty()) return 65536;
-    double d1 = 0, d2 = 0;
-    for(it = m1.begin(); it != m1.end(); it++){
-        if(m2[it->first] > 0){
+    for(map <string, int> ::iterator it = m1.begin(); it != m1.end(); it++){
+        if(m2[it->first] != 0){
             if(it->second > m2[it->first]){
-                d2 += it->second;
-                d1 += m2[it->first];
+                r += m2[it->first];
+                a += it->second;
             }
             else{
-                d1 += it->second;
-                d2 += m2[it->first];
+                a += m2[it->first];
+                r += it->second;
             }
+            m2[it->first] = 0;
         }
-        else d2 += it->second;
+        else{
+            a += it->second;
+        }
     }
-    for(it = m2.begin(); it != m2.end(); it++){
-        if(m1[it->first] == 0) d2 += it->second;
+    for(map <string, int> ::iterator it = m2.begin(); it != m2.end(); it++){
+        a += it->second;
     }
-    answer = (d1/d2) * 65536;
-    return answer;
+    double d = (double)r / (double)a;
+    return answer = d * 65536;
 }
